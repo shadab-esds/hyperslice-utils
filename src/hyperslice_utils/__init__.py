16e52494ce8519c513e8a5f46af68aa054343f5e
shadab.khan <shadab.khan@esds.co.in>
2023-11-02 17:27:08 +0530

    Revert "WIP"
    This reverts commit e9a9280e9236db6e725a9d27875e2fa575d1bd5c.

---
+++ src/hyperslice_utils/__init__.py
@@ -0,0 +1,9 @@
+"""The Hyperslice API service."""
+
+from hyperslice_utils.utils.startup import (
+    collect_role_ids,
+    setup_jwt_manager,
+    collect_user_resource_ids,
+)
+
+__version__ = "5.0.1"
---
+++ src/hyperslice_utils/schemas/__init__.py
@@ -0,0 +1,2 @@
+"""This exports all of the schemas used by the application."""
+from hyperslice_utils.schemas.formio_roles import FormioRoleSchema
